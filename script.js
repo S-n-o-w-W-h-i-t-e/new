bbf9602385e577f5f436f200e5b278651d8bd238
S-n-o-w-W-h-i-t-e <131187328+S-n-o-w-W-h-i-t-e@users.noreply.github.com>
2026-02-03 21:39:09 +0530

    App stuff

--- script.js
+++ script.js
@@ -734,3 +734,10 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered!'))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
